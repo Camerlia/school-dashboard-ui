5f26e3dd00bb32262ec4c1f73741af7be0ff1b96
Camelia <cameliaafolabi@gmail.com>
2026-02-09 07:03:54 +0100

    feat: modified the admin page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        lamaSky: "#0B2D72 ",
+        lamaSkyLight: "#0992C2",
+        lamaPurple: "#15173D",
+        lamaPurpleLight: "#982598",
+        lamaYellow: "#FDC3A1",
+        lamaYellowLight: "#FAB95B",
+      }
     },
   },
   plugins: [],
